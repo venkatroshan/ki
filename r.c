5b1c78be13affda9e636fac6b856cf94e01ce167
venkatroshan <35727735+venkatroshan@users.noreply.github.com>
2018-02-27 02:22:39 -0800

    Create r.c

---
+++ r.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,n,a,d,sum=0;
+printf("\n Enter the number of arithmetic progression");
+scanf("%d",&n);
+printf("\n Enter a and d values");
+scanf("%d%d",&a,&d);
+for(i=1;i<=n;i++)
+{
+sum=sum+(a+(i*d));
+}
+  printf("\nResult is %d",sum);
+return 0;
+}
